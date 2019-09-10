4d756b25664551425ed5415534959ce2e94f404f
Shelby Byrd <shelbybyrd@protonmail.com>
2019-09-10 15:26:50 -0700

    alerts user the welcome message

---
+++ js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var usersName = prompt('What is your name?');
+console.log('User\'s name is ' + usersName +' and they\'re AWESOME!');
+alert('HEY, ' + usersName + '! You\'re AWESOME!!!');
+
