cb1d4440abb3aa243ea9cd4edfbf064abfb7a96e
Shelby Byrd <shelbybyrd@protonmail.com>
2019-09-10 15:49:19 -0700

    Ask user if they like cats

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-var usersName = prompt('What is your name?');
+var usersName = prompt('What\'s your name?');
 console.log('User\'s name is ' + usersName +' and they\'re AWESOME!');
 alert('HEY, ' + usersName + '! You\'re AWESOME!!!');
 
+var likeCats = prompt('Do you like cats? Yes/No');
+switch(likeCats.toLowerCase()){
+case 'yes':
+  console.log(usersName + ' typed Yes');
+  alert('You like cats! Did we just become best friends?');
+  break;
+case 'no':
+  console.log(usersName + ' typed No');
+  alert('You don\'t like cats and that makes me sad...');
+  break;
+default:
+  console.log(usersName + ' typed something different than Yes or No');
+  alert('Please type Yes or No');  
+}
